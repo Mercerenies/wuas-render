f0df32a35cd94bb61de05ba7f9f28a922042c26c
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2018-04-03 16:05:58 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+This is the rendering script used for my online game Wish Upon A
+Star. Details about the game itself can be
+found [on my website](http://mercerenies.sdf.org/wuas/) (mirrored as a
+repo [here](https://github.com/Mercerenies/personal-website)). This
+repository contains the rendering Python script, as well as a few
+example data files.
---
+++ render.py
@@ -0,0 +1,253 @@
+#!/usr/bin/python3
+
+from PIL import Image
+from sys import argv, stdout
+import json
+import numpy as np
+
+class Space:
+    """A space consists of an identifier, a (possibly empty) iterable
+    sequence of references, and a number of flags.
+
+    """
+
+    def __init__(self, space, refs):
+        """Constructs a space given a (possibly tagged) identifier and a
+        sequence of references. If the tagged space identifier ends
+        with a question mark, the space will be hidden and the
+        identifier will not contain the question mark. If the
+        identifier is the empty string, it defaults to "gap". If the
+        identifier is "nil", it is translated to None.
+
+        """
+        self.space = space_abbr(space)
+        self.refs = refs
+        self.show = not space.endswith("?")
+
+    def __str__(self):
+        return "Space({!r}, {!r})".format(self.space, self.refs)
+
+    def __repr__(self):
+        return str(self)
+
+    def image(self, spaces):
+        """Looks up the image for the space, given the configuration
+        information, and returns a Pillow image object. The argument
+        should be the image containing all of the spaces. The global
+        configuration dictionary will be used to determine which part
+        of the image to crop out. If the space identifier is None or
+        the space is hidden, None is returned.
+
+        """
+        if self.space is None or not self.show:
+            return None
+        coords = dictionary['spaces'][self.space]['coords']
+        x, y, w, h = tuple(map(int, coords.split(",")))
+        return spaces.crop((x, y, w, h))
+
+    def layer(self):
+        """Returns the layer of this space. The layer of a space is 0 if it is
+        a gap and 1 otherwise.
+
+        """
+        return space_layer(self.space)
+
+class Ref:
+    """References are stored for items and tokens in the game. A reference
+    consists of an identifier, an item identifier (which might be
+    "nil"), and a relative position within the space. If the item
+    identifier is non-nil, then the full identifier should be "item".
+
+    """
+
+    def __init__(self, name, item, pos):
+        self.name = name
+        self.item = item
+        self.pos = pos
+
+    def __str__(self):
+        return "Ref({!r}, {!r})".format(self.name, self.item, self.pos)
+
+    def __repr__(self):
+        return str(self)
+
+    def image(self, tokens):
+        """Given the image of tokens, produces a cropped variant which
+        consists only of the given token.
+
+        """
+        x, y = dictionary['tokens'][self.name]['thumbnail']
+        return tokens.crop((x, y, x + 16, y + 16))
+
+with open("config.json") as main_file:
+    config = json.load(main_file)
+    with open(config['files']['dict']) as dict_file:
+        dictionary = json.load(dict_file)
+
+def spaces_png():
+    """Opens the spaces image and returns it as a Pillow object."""
+    return Image.open(config['files']['spaces'])
+
+def tokens_png():
+    """Opens the tokens image and returns it as a Pillow object."""
+    return Image.open(config['files']['tokens'])
+
+def space_abbr(space):
+    """If the identifier ends with a ?, this function strips the question
+    mark. If the identifier is "nil", it is translated to None. If the
+    identifier is "", it is translated to "gap".
+
+    """
+    if space.endswith("?"):
+        space = space[:-1]
+    if space == "nil":
+        return None
+    elif space == "":
+        return "gap"
+    else:
+        return space
+
+def space_layer(space):
+    """Returns the layer of the space. All non-gap, non-None spaces are in
+    layer 1. Gaps always fall in layer 0. The None space has no layer,
+    so this function will return None in that case.
+
+    """
+    if space is None:
+        return None
+    elif space == "gap":
+        return 0
+    else:
+        return 1
+
+WIDTH = 32
+HEIGHT = 32
+
+def load_table(fname):
+    """Given a filename, loads the file as a table file containing the
+    board spaces and references. A 2-tuple is returned, containing the
+    numpy array representing the board and a dictionary of Ref
+    objects, with the abbreviations as the keys. Spaces whose
+    identifier contains a "*" will have the asterisk stripped, as the
+    asterisk is primarily intended for the GM's notes.
+
+    """
+
+    with open(fname) as infile:
+        while infile.readline().startswith("#"):
+            pass # Ignore leading comments
+
+        # Version
+        version = int(infile.readline())
+        if version != 1:
+            raise StandardError("Invalid version " + str(version))
+
+        # The board text
+        headline = infile.readline()
+        table = []
+        width = headline.count("+") - 1
+        while True:
+            curr = []
+            spaces_arr = infile.readline()
+            if spaces_arr.strip() == "":
+                break
+            spaces_arr = map(str.strip, spaces_arr.split("|")[1:-1])
+            tokens_arr = map(str.strip, infile.readline().split("|")[1:-1])
+            for v, z in zip(spaces_arr, tokens_arr):
+                v = v.replace("*", "")
+                curr.append(Space(v, list(z)))
+            table.append(curr)
+            infile.readline() # The next header
+
+        # Token references
+        refs = {}
+        while True:
+            curr = infile.readline()
+            if curr.strip() == "":
+                break
+            ref, name, item, x, y = curr.split()
+            refs[ref] = Ref(name, item, (int(x), int(y)))
+
+        return (np.array(table), refs)
+
+
+def render_image(table, refs):
+    """Given the output of load_table, renders the various layers as a
+    Pillow image and returns the image object.
+
+    """
+    with spaces_png() as spaces, tokens_png() as tokens:
+
+        # Instantiate the image
+        height, width = table.shape
+        result = Image.new("RGBA", (width * WIDTH, height * HEIGHT))
+
+        # Gap layer
+        for y, row in enumerate(table):
+            for x, elem in enumerate(row):
+                if elem.layer() == 0:
+                    img = elem.image(spaces)
+                    if img is not None:
+                        result.paste(img, (x * WIDTH, y * HEIGHT), img)
+
+        # Normal space layer
+        for y, row in enumerate(table):
+            for x, elem in enumerate(row):
+                if elem.layer() == 1:
+                    img = elem.image(spaces)
+                    if img is not None:
+                        result.paste(img, (x * WIDTH, y * HEIGHT), img)
+
+        # Ref layer
+        for y, row in enumerate(table):
+            for x, elem in enumerate(row):
+                for ref in elem.refs:
+                    img = refs[ref].image(tokens)
+                    dx, dy = refs[ref].pos
+                    if img is not None:
+                        result.paste(img, (x * WIDTH + dx, y * HEIGHT + dy), img)
+
+        return result
+
+def render_json(table, refs):
+    """Given the output of load_table, renders the JSON output necessary
+    for compatibility with the interactive board.
+
+    """
+    # TODO Multi-spaces aren't handled here right now
+    spaces = []
+    tokens = []
+    for y, row in enumerate(table):
+        curr = []
+        for x, elem in enumerate(row):
+            curr.append(elem.space or "gap")
+            for ref in elem.refs:
+                name = refs[ref].name
+                dx, dy = refs[ref].pos
+                if name == "item":
+                    name = refs[ref].item
+                obj = {
+                    'object': name,
+                    'position': [x * WIDTH + dx, y * HEIGHT + dy]
+                }
+                tokens.append(obj)
+        spaces.append(curr)
+    data = {
+        '0': {'spaces': spaces, 'tokens': tokens}
+    }
+    return data
+
+if __name__ == "__main__":
+    setting = argv[1]
+    infile = argv[2]
+    table, refs = load_table(infile)
+    if setting == '-i':
+        with render_image(table, refs) as img:
+            if len(argv) > 3:
+                img.save(argv[3])
+            else:
+                img.show()
+    elif setting == '-t':
+        data = render_json(table, refs)
+        json.dump(data, stdout)
+        print()
